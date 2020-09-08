0c924a9e4adc71a831a2d6aea818fa8e56eb79f3
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-09-08 15:22:49 +0100

    step 8 done

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,36 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   DEFAULT_VALUE = 15
+  DEFAULT_LIMIT = 90
 
   def initialize(balance = DEFAULT_VALUE)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(amount)
+    fail "Limit is £#{DEFAULT_LIMIT}" if @balance + amount > DEFAULT_LIMIT
+    @balance += amount
 
-  @balance += amount
+  end
 
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -16,5 +16,47 @@ describe Oystercard do
   expect {oyster.top_up(10)}.to change{oyster.balance}.by 10
   end
 
+  it "sends an error message if the card is topped up with more than £90" do
+
+    oyster = Oystercard.new
+
+    expect {oyster.top_up(76)}.to raise_error "Limit is £90"
+  end
+
+  it "checks that you can deduct money from  the balance" do
+    oyster = Oystercard.new
+
+    expect {oyster.deduct(10)}.to change{oyster.balance}.by -10
+  end
+
+  describe 'touch_in' do
+    it "checks that the oyster card has been touched in" do
+      oyster = Oystercard.new
+      oyster.touch_in
+      expect(oyster.in_journey).to eq true
+    end
+  end
+
+  describe 'touch out' do
+    it "checks that the oyster card gas been touched out" do
+      oyster = Oystercard.new
+      oyster.touch_in
+      oyster.touch_out
+      expect(oyster.in_journey).to eq false
+    end
+  end
+
+  describe 'in_journey?' do
+    it "checks whether the card is in use or not" do
+      oyster = Oystercard.new
+      oyster.touch_in
+      expect(oyster.in_journey?).to be_truthy
+    end
+    it "checks whether the card is in use or not" do
+      oyster = Oystercard.new
+      oyster.touch_out
+      expect(oyster.in_journey?).to be_falsey
+    end
+  end
 
   end
